a07995dcaf6b2c0026fb6b73b0906a9d66645cfa
EvaLiho <165931881+EvaLiho@users.noreply.github.com>
2024-04-24 23:11:17 +0300

    Update Week6.java

--- Week6.java
+++ Week6.java
@@ -25,3 +25,59 @@ public class Main {
     }
   }
 
+import java.util.Scanner; 
+public class Main {                        
+  public static void main(String[] args) { 
+
+    int number1 = input();
+    int number2 = input();
+    
+    int result = sum(number1, number2);
+    System.out.println(result);
+     
+  }
+  
+  public static int input() {
+    System.out.println("Please enter number: ");
+    Scanner scanner = new Scanner(System.in); 
+    return scanner.nextInt();
+    
+    }
+    
+  public static int sum(int number1, int number2) {
+    return number1 + number2;
+  }
+  
+}
+
+import java.util.Scanner;
+public class Main {                        
+  public static void main(String[] args) { 
+  
+    System.out.println("Please enter a title: ");
+    String title = getTitle();
+    System.out.println("Please enter a paragtaph: ");
+    String paragraph = getParagraph();
+    int titleLength = title.length();
+    String border = "=".repeat(titleLength);
+
+    printBook(title, paragraph, border);
+  }
+  public static String getTitle() {
+    Scanner scanner = new Scanner(System.in); 
+    String title = scanner.nextLine(); 
+    return title;
+  }
+  public static String getParagraph() {
+      Scanner scanner = new Scanner(System.in); 
+      String paragraph = scanner.nextLine(); 
+      return paragraph;
+  }
+  public static void printBook(String title, String paragraph, String border){
+    System.out.println(border);
+    System.out.println(title);
+    System.out.println(border);
+    System.out.println(paragraph); 
+    
+  }
+}
