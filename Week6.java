5cb61ac6af34689feddec2a6bd9fd85ad73f4c02
EvaLiho <165931881+EvaLiho@users.noreply.github.com>
2024-04-25 19:31:08 +0300

    Update Week6.java

--- Week6.java
+++ Week6.java
@@ -25,7 +25,31 @@ public class Main {
     }
   }
 
-import java.util.Scanner; 
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    
+    String[] partyList = {"Oskars", "Maris", "Andrea"};
+
+    Scanner scanner = new Scanner(System.in);
+    String name = scanner.nextLine();
+    
+    boolean isInvited = false;
+    
+    for (int i = 0; i < partyList.length; i++){
+      if(partyList[i].equals(name)){
+        isInvited = true;
+        System.out.println(name + " is invited");
+        break;
+      }
+    }
+
+    if(!isInvited){
+      System.out.println(name + " is not invited");
+    }
+  }
+}import java.util.Scanner; 
 public class Main {                        
   public static void main(String[] args) { 
 
@@ -81,3 +105,103 @@ public class Main {
     
   }
 }
+import java.util.Scanner;
+public class Main {                        
+  public static void main(String[] args) { 
+  
+    System.out.println("Please enter a title: ");
+    String title = getText();
+    System.out.println("Please enter a paragtaph: ");
+    String paragraph = getText();
+    int titleLength = title.length();
+    String border = "=".repeat(titleLength);
+
+    printBook(title, paragraph, border);
+  }
+  public static String getText() {
+    Scanner scanner = new Scanner(System.in); 
+    return scanner.nextLine();
+  }
+  public static void printBook(String title, String paragraph, String border){
+    System.out.println(border);
+    System.out.println(title);
+    System.out.println(border);
+    System.out.println(paragraph); 
+  }
+}
+import java.util.Scanner;
+public class Main {                        
+  public static void main(String[] args) { 
+  
+    System.out.println("Please enter a title: ");
+    String title = getText();
+  
+  }
+  public static String getText() {
+    Scanner scanner = new Scanner(System.in); 
+    return scanner.nextLine();
+  }
+}
+
+----------------------------------------------------------------------------
+public class Main {                        
+  public static void main(String[] args) { 
+
+    int number = 40;
+    checkNumber(number);
+    int number2 = 60;
+    checkNumber(number2);
+  }
+    public static void checkNumber(int number){
+      if(number > 50){
+       System.out.println("Number is greater than 50");
+      }else if(number < 50){
+        System.out.println("Number is less than 50");
+      }else{
+        System.out.println("Number is equal to 50");  
+      }
+    }
+}
+------------------------------------------------------------------------------
+public class Main {                        
+  public static void main(String[] args) { 
+
+    int number = 40;
+    String result = checkNumber(number);
+    System.out.println(result);
+    int number2 = 60;
+    String result2 = checkNumber(number2);
+    System.out.println(result2);
+  }
+    public static String checkNumber(int number){
+      if(number > 50){
+       return "Number is greater than 50";
+      }else if(number < 50){
+        return "Number is less than 50";
+      }else{
+        return "Number is equal to 50";  
+      }
+    }
+}
+--------------------------------------------------------------------------------
+  public class Main {                        
+  public static void main(String[] args) { 
+
+    int number = 40;
+    String result = checkNumber(number);
+    System.out.println(result);
+    int number2 = 60;
+    String result2 = checkNumber(number2);
+    System.out.println(result2);
+  }
+    public static String checkNumber(int number){
+      if(number > 50){
+       return number + " is greater than 50";
+      }else if(number < 50){
+        return number + " is less than 50";
+      }else{
+        return number + " is equal to 50";  
+      }
+    }
+}
+---------------------------------------------------------------------------------
