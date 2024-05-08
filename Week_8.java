cad8209daca23dcc0aca37ec5355c7004a2bda30
EvaLiho <165931881+EvaLiho@users.noreply.github.com>
2024-05-08 18:49:17 +0300

    Update Week_8.java

--- Week_8.java
+++ Week_8.java
@@ -1,1 +1,25 @@
+PLACE IN THE RACE:
 
+ public class Main {
+  public static void main(String[] args) {
+    
+    int[] arr = {8, 7, 5, 3, 2, 1};
+    int number = 0;
+    boolean thePlaceIsFound = false;
+    
+    for(int i = 0; i < arr.length; i++){
+      if(arr[i] < number){
+        i = i + 1;
+        System.out.println("Our place in the race: " + i);
+        thePlaceIsFound = true;
+        break;
+      }
+    }
+    
+    if(thePlaceIsFound == false){
+      System.out.println("Our place in race: " + (arr.length + 1)); 
+    }
+  }
+}
+___________________________________________________________________________________
+  
