fd795b69d82173503bffb3aa17238e0428df0868
EvaLiho <165931881+EvaLiho@users.noreply.github.com>
2024-05-08 19:22:55 +0300

    Update Week_8.java

--- Week_8.java
+++ Week_8.java
@@ -22,4 +22,46 @@ PLACE IN THE RACE:
   }
 }
 ___________________________________________________________________________________
+SAME WITH METHODS
+
+public class Main {
+  public static void main(String[] args) {
+    
+    int[] arr = {8, 7, 5, 3, 2, 1};
+    int number = 0;
+    int place = getThePlace(arr, number);
+      System.out.println("Our place in race: " + place); 
+    
+  }
+  public static int getThePlace(int[] arr, int number){
+    for(int i = 0; i < arr.length; i++){
+      if(arr[i] < number){
+        return i+1;
+      }
+    }
+    return arr.length +1;
+  }
+}
+________________________________________________________________________________________
+
+ IS PRIME NUMBER?
+
+ public class Main {
+  public static void main(String[] args) {
+
+    for(int i = 0; i < 100; i++){
+      boolean isAPrimeNumber = isPrime(i);
+      System.out.println(i + "is a prime number - " + isAPrimeNumber);
+    }
+  }
   
+  public static boolean isPrime(int number){
+    for(int i= 2; i < number; i++){
+      if(number % i == 0){
+        return false;
+      }
+    }
+    return true;
+  }
+}
+_____________________________________________________________________________________________
