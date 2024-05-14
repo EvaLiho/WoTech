365f459ddfc3e22afea9558cd75a9465b2cac84b
EvaLiho <165931881+EvaLiho@users.noreply.github.com>
2024-05-14 17:52:20 +0300

    Update Week_8.java

--- Week_8.java
+++ Week_8.java
@@ -65,3 +65,44 @@ ________________________________________________________________________________
   }
 }
 _____________________________________________________________________________________________
+
+ TWO DIMENSIONAL ARRAY
+
+public class Main {
+  public static void main(String[] args) {
+    int[][] array = {
+      {1, 2, 3},
+      {4, 5, 6},
+      {7, 8, 9}
+    };
+
+for(int i = 0; i < array.length; i++){
+  for(int j = 0; j < array[i].length; j++){
+    System.out.print(array[i][j] + " ");
+  }
+
+}
+ 
+}
+}
+______________________________________________________________________________________________
+ public class Main {
+  public static void main(String[] args) {
+    int[][] array = new int[5][5];
+   
+    for (int i = 0; i < array.length; i++) {
+      int[] row = array[i];
+      for (int j = 0; j < row.length; j++) {
+          row[j] = i;
+     }
+     }
+   
+    for(int i = 0; i < array. length; i++){
+      for(int j = 0; j < array[i].length; j++){
+        System.out.print(array[i][j] + "|");
+      }
+    System.out.println(); // FOR A NEW ROW IN PRINT
+    }
+    }
+    
+}
