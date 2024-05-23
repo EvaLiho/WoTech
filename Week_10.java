4c7351c5963498115d499dcdee41e1443b54fee2
EvaLiho <165931881+EvaLiho@users.noreply.github.com>
2024-05-23 19:36:07 +0300

    Update Week_10.java

--- Week_10.java
+++ Week_10.java
@@ -170,3 +170,208 @@ public class Main {
     }
   }
 __________________________________________________________________________________________________________________________________________
+        
+import java.util.ArrayList;
+
+public class Main {
+  public static void main(String[] args) {
+    
+    ArrayList<String> shopsItems = new ArrayList<String>();
+
+    
+    shopsItems.add("Table");
+    shopsItems.add("Cupboard");
+    shopsItems.add("Chair");
+    shopsItems.add("Couch");
+    shopsItems.add("Coffee table");
+    shopsItems.add("Glass table");
+
+    
+    printArrayList(shopsItems);
+    System.out.println("__________________");
+    
+      var filteredArrayList = new ArrayList<String>();
+    for(String item: shopsItems){
+      if(item.contains("table")){                                // CONTAIN function for ArrayList
+        filteredArrayList.add(item);
+      }
+    }
+    printArrayList(filteredArrayList);
+  
+  }
+
+    public static void printArrayList(ArrayList<String> items){
+      for (String item : items){
+        System.out.println(item);
+      }
+    }
+  }
+__________________________________________________________________________________________________________________
+        
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    
+    ArrayList<String> shopsItems = new ArrayList<String>();
+
+    
+    shopsItems.add("Table");
+    shopsItems.add("Cupboard");
+    shopsItems.add("Chair");
+    shopsItems.add("Couch");
+    shopsItems.add("Coffee table");
+    shopsItems.add("Glass table");
+
+    
+    printArrayList(shopsItems);
+    System.out.println("__________________");
+
+    var filteredArrayList = shopsItems
+      .stream()                                  //
+      .filter(item -> item.contains("table"))    // if TRUE it returns item
+      //.skip(3)                                    // skips 3 items  
+      //.limit(2)                                    // takes 2 items
+      .collect(Collectors.toList());             // collects all filtered items into a list
+
+    System.out.println(filteredArrayList);
+    //printArrayList(filteredArrayList);
+  
+  }
+
+    public static void printArrayList(ArrayList<String> items){
+      for (String item : items){
+        System.out.println(item);
+      }
+    }
+  }
+________________________________________________________________________________________________________
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    
+    ArrayList<String> shopsItems = new ArrayList<String>();
+
+    shopsItems.add("Table");
+    shopsItems.add("Cupboard");
+    shopsItems.add("Chair");
+    shopsItems.add("Couch");
+    shopsItems.add("Coffee table");
+    shopsItems.add("Glass table");
+
+    printArrayList(shopsItems);
+    System.out.println("__________________");
+
+   shopsItems
+      .stream() 
+      .filter(x -> x.contains("table"))
+      .forEach(x -> System.out.println("test "+x));                       // forEach function for STREAM
+     }
+    public static void printArrayList(ArrayList<String> items){
+      for (String item : items){
+        System.out.println(item);
+      }
+    }
+  }
+___________________________________________________________________________________________________
+        import java.util.ArrayList;
+import java.util.stream.Collectors;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    
+    ArrayList<String> shopsItems = new ArrayList<String>();
+
+    shopsItems.add("Table");
+    shopsItems.add("Cupboard");
+    shopsItems.add("Chair");
+    shopsItems.add("Couch");
+    shopsItems.add("Coffee table");
+    shopsItems.add("Glass table");
+    
+    printArrayList(shopsItems);
+    System.out.println("__________________");
+
+   shopsItems
+      .stream() 
+      .filter(x -> x.contains("table"))
+      .forEach(x -> print(x));
+   }
+
+    public static void printArrayList(ArrayList<String> items){
+      for (String item : items){
+        System.out.println(item);
+      }
+    }
+    public static void print(String text){                                        // function to PRINT extra empty line
+      System.out.println();
+      System.out.println(text);
+    }
+  }
+_______________________________________________________________________________
+// GROUP WORK - EASY - Create an integer ArrayList, skip the first 5 numbers. 
+// And then with the rest of them, print them out.
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+import java.util.Random;
+
+public class Main {
+  public static void main(String[] args) {
+    
+    var listOfNumbers = new ArrayList<Integer>();
+
+    addRandomNumber(listOfNumbers);
+    
+    var filteredList = listOfNumbers
+      .stream() 
+      .skip(5)
+      .collect(Collectors.toList());             
+
+    System.out.println(filteredList);
+   }
+  public static void addRandomNumber(ArrayList<Integer> listOfNumbers) {
+      Random random = new Random();
+      for (int i = 0; i < 10; i++) {
+          int randomNumber = random.nextInt(100);
+            listOfNumbers.add(randomNumber);
+      }
+  }
+}
+____________________________________________________________________________________________
+// GROUP WORK - Medium: Create an integer ArrayList, make sure they divide by 2,
+// skip the first 5, limit to three and print them out from another function.    
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+import java.util.Random;
+
+public class Main {
+  public static void main(String[] args) {
+    
+    var listOfNumbers = new ArrayList<Integer>();
+
+    addRandomNumber(listOfNumbers);
+    
+    var filteredList = listOfNumbers
+      .stream() 
+      .filter(number -> number % 2 == 0)
+      .skip(5)
+      .collect(Collectors.toList());             
+
+    System.out.println(filteredList);
+   }
+  public static void addRandomNumber(ArrayList<Integer> listOfNumbers) {
+      Random random = new Random();
+      for (int i = 0; i < 10; i++) {
+          int randomNumber = random.nextInt(100);
+            listOfNumbers.add(randomNumber);
+      }
+  }
+}
+
+        
