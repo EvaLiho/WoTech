8eb074f8c97d924e176a728472917f75039c4bdf
EvaLiho <165931881+EvaLiho@users.noreply.github.com>
2024-05-29 20:02:34 +0300

    Add files via upload

---
+++ Lesson_20_Teamwork/Plant.java
@@ -0,0 +1,13 @@
+public class Plant {
+  public String name;
+  public Double size;
+  public int age;
+
+  public Plant(String name, Double size, int age){
+    this.name = name;
+    this.size = size;
+    this.age = age;
+  }
+  public Plant(){
+  }
+}
