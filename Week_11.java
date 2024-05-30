6ceb07ce78807b28096653f8ad2ce3a29ad054bc
EvaLiho <165931881+EvaLiho@users.noreply.github.com>
2024-05-30 20:11:33 +0300

    Update Week_11.java

--- Week_11.java
+++ Week_11.java
@@ -118,3 +118,150 @@ FILE PLANT
   }
 }
 _________________________________________________________________________________________________________________________
+MAIN FILE
+
+  // NO MORE STATIC except in main once
+public class Main {
+  public static void main(String[] args) {      // main method
+    Student.studentCount = 10;
+
+    
+    Student student = new Student();
+    student.name = "john";
+    Student student2 = new Student();
+
+    System.out.println(student2.name);
+    System.out.println(Student.studentCount);    // static can be with class
+    // Main.main(args); -> "stack overflow"
+  }
+}
+
+STUDENT FILE
+  public class Student{
+  public static int studentCount = 0;    // default is 0 so it is not needed
+  public String name;    // non-static property
+  public int age;        
+  
+}
+___________________________________________________________________________________________________________________
+   FILE MAIN
+
+public class Main {
+  public static void main(String[] args) {   
+
+    Square square1 = new Square(3);
+    var square1Perimeter = square1.getPerimeter();  // var is not mandatory
+    System.out.println(square1Perimeter);
+
+    Square square2 = new Square(5);
+    System.out.println(square2.getPerimeter());  
+
+    Square square3 = new Square(3);
+    var square3Area = square3.getArea();  // var is not mandatory
+    System.out.println(square3Area);
+  }
+}
+
+FILE SQUARE
+public class Square{
+  public int sideLength;
+  public Square(int sideLength){
+    this.sideLength = sideLength;
+  }
+
+  public int getPerimeter(){
+    return sideLength * 4;
+  }
+
+  public int getArea(){
+    return sideLength * sideLength;
+}
+}
+______________________________________________________________________________________________________________
+  FILE MAIN
+  public class Main {
+    public static void main(String[] args) {
+        var fork = new Fork(4, "Silver");
+
+        System.out.println("The fork made out of " + fork.material + " has " +  fork.spikeCount + " spikes in total. And the sharpness is " + fork.sharpness);
+
+        System.out.println("The fork is made out of " + fork.material);
+
+        System.out.println("The fork has " + fork.spikeCount + " spikes in total.");
+
+        System.out.println("And the sharpness is " + fork.sharpness);
+
+        // we will create a stabbing machine. 
+
+        for(int i = 0; i < 100; i++){
+            fork.stab();
+        }
+        System.out.println("The sharpness after 100 stabs is " + fork.sharpness);
+    }
+}
+FILE FORK
+  //Fork has 3 or 4  spikes
+//Fork has a material (silver, or plastic, wood)
+//Fork has sharpness in the spikes. 0
+//A fork can stab and whenever a fork stabs, it gets more dull
+//Sharpness is gonna decrease by 0.1
+
+public class Fork{
+  public int spikeCount;
+  public String material;
+  public int sharpness = 1000;
+
+  //new Fork(3, "wood")
+  public Fork(int spikeCount, String material){
+    this.spikeCount = spikeCount;
+    this.material = material;
+  }
+
+  public void stab(){
+    sharpness = sharpness - 1;
+  }
+}
+____________________________________________________________________________________________________
+  TEAMWORK
+
+  MAIN FILE
+  //Choose an item of your choosing and create a class.
+//Define 3 different properties (same as yesterday)
+//Define a function (action that you can do with this item) which would
+//change one of the properties value
+
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Tree tree = new Tree("Willow", 24.5);
+
+        System.out.println("This tree is a " + tree.treeName + " tree and it is " +  tree.treeHeight + "m high ");
+
+
+        for(int i = 0; i < 1000; i++){
+            tree.autumn();
+        }
+        System.out.println("When Autumn comes and leaves start to fall it will have " + tree.leaves +" leaves left");
+    }
+}
+TREE FILE
+  public class Tree{
+  public String treeName;
+  public Double treeHeight;
+  public int leaves = 10000;
+
+  public Tree(String treeName, Double treeHeight){
+    this.treeName = treeName;
+    this.treeHeight = treeHeight;
+  }
+
+  public void autumn(){
+    leaves = leaves - 1;
+  }
+}
+
+
+
+
+  
